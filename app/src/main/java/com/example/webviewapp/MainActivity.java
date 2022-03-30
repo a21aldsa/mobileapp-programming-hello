4053967ea1d35ac49c59383e420e07b5b76edd5e
a21aldsa <a21aldsa@student.his.se>
2022-03-30 16:22:11 +0200

    Ändrade namn på app Lade till Toast message och Logcat meddelanden.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.webviewapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String toast_thing = "nämennn";
+        int test_tal = 555;
+
+        Log.d("idk_debuggg","Före toast.");
+        Toast.makeText(getApplicationContext(), toast_thing+String.valueOf(test_tal), Toast.LENGTH_LONG).show();
+        Log.d("idk_debuggg","Efter toast.");
     }
 
 }
